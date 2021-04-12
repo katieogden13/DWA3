53b28b0f6178a20b300458f76ab68308d1c14126
Katie Ogden <katie@Katies-MacBook-Pro.local>
2021-04-12 11:09:57 -0600

    updated format in js

--- js/scripts.js
+++ js/scripts.js
@@ -24,36 +24,22 @@ function setup(){
     createCanvas(900, 500);
     // hue, saturation, and brightness
     colorMode(HSB, 255);
-    // slider has a range between 0 and 255 with a starting value of 127
-//    slider = createSlider(0, 255, 127);
-    
-    
+//    background('#6F8695');
+  
 }
 
 function draw(){
     
 // Reference code here
-        background(127);
-        strokeWeight(2);
+    strokeWeight(2);
     
-    // Set the hue according to the slider
-        stroke(slider.value(), 255, 255);
-        fill(slider.value(), 255, 255, 127);
-        ellipse(360, 200, 200, 200);
-//
-    
-    
-    
-    //background(255,10);
     translate(vidcapture.width, 0);
     scale (-1, 1);
     
     //image(vidcapture, 0, 0)
-    
     var positions = ctracker.getCurrentPosition()
     
     //print(positions);
-    
     if (positions) {
         
         // all the fun with positions in here!
@@ -69,39 +55,22 @@ function draw(){
 //        line(positions[28][0], positions[28][1], positions[28][0]+30, positions[28][1]+30);
         
         
-        print("x: " + positions[62][0])
-        print("y: " + positions[62][1])
+//        print("x: " + positions[62][0])
+//        print("y: " + positions[62][1])
         
-        var r = map(positions[62][0], 250, 300, 0, 255, true)
-        var b = map(positions[62][1], 150, 200, 0, 255, true)
         
-        background(r, 255, b)
+        var r = map(positions[62][0], 0, width, 0, 255)
+        var b = map(positions[62][1], 0, height, 0, 255)
+        
+        fill(r, 255, b);
+        ellipse(360, 200, 200, 200);
         tint(255, 126)
-        image(vidcapture, 0, 0)
+        //VIDEO FEED 
+//        image(vidcapture, 0, 0)
         
+//        fill('#6F8695');
+//        rectangle(100, 100, 100, 100)
+      
     }
-    
-    
-    
-    
-//    translate(mouseX, mouseY);
-////    noFill()
-//    noStroke();
-//    fill(255, 204, 0);
-////    circle(mouseX, mouseY, 40)
-////    print("x:" + mouseX)
-////    print("y:" + mouseY)
-//    
-//    var xColor = map(mouseX, 0, width, 0, 255)
-//    var yColor = map(mouseY, 0, height, 0, 255)
-//    
-//    stroke(xColor, 0, yColor);
-//    
-//    var xRotate = map(mouseX, 0, width, 0, 360)
-//    print(xRotate)
-//    
-//    angleMode(DEGREES)
-//    rotate(xRotate);
-//    line(0, 0, 50, 50)
-//      
+        
 }
